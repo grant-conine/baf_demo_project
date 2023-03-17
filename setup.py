f79674d62850c72cc065dbec9a672cdbfa053406
Grant Conine <gconine@gmail.com>
2023-03-16 23:58:55 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A demo of reproducible data science on the BAF NeurIPS 2022 dataset',
+    author='Grant Conine',
+    license='MIT',
+)
