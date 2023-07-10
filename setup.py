bc51f0b8903bac900f6785cf219c961e778bbbc4
Grant Conine <gconine@gmail.com>
2023-07-10 22:09:46 +0000

    Added pyproject.toml

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='A demo of reproducible data science on the BAF NeurIPS 2022 dataset',
-    author='Grant Conine',
-    license='MIT',
-)
+setup()
